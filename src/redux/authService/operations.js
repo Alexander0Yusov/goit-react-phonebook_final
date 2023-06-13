7356a3f2569f834af67f01c8bd577b723a10fe91
Alexander <yusovsky2@gmail.com>
2023-06-13 18:25:15 +0300

    try without axios instance

--- src/redux/authService/operations.js
+++ src/redux/authService/operations.js
@@ -1,13 +1,20 @@
 import { axiosInstance, pushToken, pullToken } from 'redux/axiosHerokuInstance';
+import axios from 'axios';
 
 export const signUp = async user => {
-  const { data } = await axiosInstance.post('/users/signup', user);
+  const { data } = await axios.post(
+    'https:connections-api.herokuapp.com/users/signup',
+    user
+  );
   pushToken(data.token);
   return data;
 };
 
 export const login = async user => {
-  const { data } = await axiosInstance.post('/users/login', user);
+  const { data } = await axios.post(
+    'https:connections-api.herokuapp.com/users/login',
+    user
+  );
   pushToken(data.token);
   return data;
 };
